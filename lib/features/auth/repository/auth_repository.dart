c0dd18af1f87e01d16bbdbc021297a4c0b761290
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-11 23:52:45 +0900

    AuthRepositoryを作成

---
+++ lib/features/auth/repository/auth_repository.dart
@@ -0,0 +1,20 @@
+// Firebase呼び出しのロジック
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthRepository {
+  // 外部からアクセスできないようにする
+  final FirebaseFirestore _firestore;
+  final FirebaseAuth _auth;
+  final GoogleSignIn _googleSignIn;
+
+  AuthRepository({
+    required FirebaseFirestore firestore,
+    required FirebaseAuth auth,
+    required GoogleSignIn googleSignIn,
+  })  : _auth = auth,
+        _firestore = firestore,
+        _googleSignIn = googleSignIn;
+}
+
