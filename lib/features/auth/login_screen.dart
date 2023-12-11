3c1d14db25bbca8db63c7935ba90222d25992afc
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-11 19:22:10 +0900

    AppBarのtitleにlogoを追加

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+      ),
+    );
   }
 }
