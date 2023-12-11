c8890d48aa87708abd6f10855f968d4c79a74311
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-12-11 19:49:26 +0900

    Login Screen UI (#3)
    * assets/imagesに画像を追加
    
    * AppBarのtitleにlogoを追加
    
    * actionsを追加
    
    * bodyを作成

---
+++ lib/core/common/sign_in_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/core/constants/constants.dart';
+import 'package:reddit_tutorial/theme/palette.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18),
+      child: ElevatedButton.icon(
+        onPressed: () {},
+        icon: Image.asset(
+          Constants.googlePath,
+          width: 35,
+        ),
+        label: const Text(
+          'Continue with Google',
+          style: TextStyle(fontSize: 18),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Palette.grayColor,
+          minimumSize: const Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/core/common/sign_in_button.dart';
+import 'package:reddit_tutorial/core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Skip',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            'Dive into anything',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.asset(
+              Constants.loginEmotePath,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const SignInButton(),
+        ],
+      ),
+    );
   }
 }
