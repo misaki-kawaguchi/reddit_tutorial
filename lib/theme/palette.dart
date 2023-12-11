34abc82b8bdb9ccb08e7eabc9d47e3e5f372e483
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-11 18:31:58 +0900

    Colorsを追加

---
+++ lib/theme/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // Colors
+  static const blackColor = Color.fromRGBO(1, 1, 1, 1);
+  static const grayColor = Color.fromRGBO(26, 39, 45, 1);
+  static const drawerColor = Color.fromRGBO(18, 18, 18, 1);
+  static const whiteColor = Colors.white;
+  static var redColor = Colors.red.shade500;
+  static var blueColor = Colors.blue.shade300;
+}
