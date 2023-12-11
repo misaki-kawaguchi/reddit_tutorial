4174857df954128274f1e9d6cbc010a825c28c1d
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-11 20:13:59 +0900

    firebaseのセットアップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_tutorial/features/auth/login_screen.dart';
+import 'package:reddit_tutorial/firebase_options.dart';
 import 'package:reddit_tutorial/theme/palette.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
