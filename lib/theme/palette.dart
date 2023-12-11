9098838de28e69648937964abd392e572d933bc6
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-11 18:38:06 +0900

    Themesを追加

--- lib/theme/palette.dart
+++ lib/theme/palette.dart
@@ -8,4 +8,36 @@ class Palette {
   static const whiteColor = Colors.white;
   static var redColor = Colors.red.shade500;
   static var blueColor = Colors.blue.shade300;
+
+  // Themes
+  static var darkModeAppTheme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: blackColor,
+    cardColor: grayColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: drawerColor,
+      iconTheme: IconThemeData(
+        color: whiteColor,
+      ),
+    ),
+    drawerTheme: const DrawerThemeData(backgroundColor: drawerColor),
+    primaryColor: redColor,
+    backgroundColor: drawerColor,
+  );
+
+  static var lightModeAppTheme = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: whiteColor,
+    cardColor: grayColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: whiteColor,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: blackColor,
+      ),
+    ),
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: whiteColor,
+    ),
+    primaryColor: redColor,
+    backgroundColor: whiteColor,
+  );
 }
