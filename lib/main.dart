ebbc121abf168f23bdf66c7b9d9bdddd6e475ce5
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-11 18:40:09 +0900

    titleとthemeを修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/theme/palette.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Reddit Tutorial',
+      theme: Palette.darkModeAppTheme,
       home: const SizedBox(),
     );
   }
