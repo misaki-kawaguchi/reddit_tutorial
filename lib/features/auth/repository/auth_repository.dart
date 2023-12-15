1ad5204c821ff43c9d8233c79ba14fdfe4ebd1e8
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-15 17:30:58 +0900

    UserModelにログイン情報を代入

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -3,7 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_tutorial/core/constants/constants.dart';
 import 'package:reddit_tutorial/core/providers/firebase_providers.dart';
+import 'package:reddit_tutorial/models/user_model.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -40,6 +42,15 @@ class AuthRepository {
 
       UserCredential userCredential = await _auth.signInWithCredential(credential);
 
+      UserModel userModel = UserModel(
+        name: userCredential.user!.displayName ?? 'No Name',
+        profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+        banner: Constants.bannerDefault,
+        uid: userCredential.user!.uid,
+        isAuthenticated: true,
+        karma: 0,
+        awards: [],
+      );
       print(userCredential.user?.email);
     } catch (e) {
       print(e);
