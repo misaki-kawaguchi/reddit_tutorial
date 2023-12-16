67c7764207b7471b8b16ee3406eb5beb2a41815f
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-12-16 22:33:34 +0900

    Storing Data To Provider (#9)
    * ログイン情報をUserModelに保存する
    
    * ローディングを追加

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -2,19 +2,35 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_tutorial/core/utils.dart';
 import 'package:reddit_tutorial/features/auth/repository/auth_repository.dart';
+import 'package:reddit_tutorial/models/user_model.dart';
 
-final authControllerProvider = Provider(
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) => AuthController(
-    authRepository: ref.read(authRepositoryProvider),
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
   ),
 );
 
-class AuthController {
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
-  AuthController({required AuthRepository authRepository}) : _authRepository = authRepository;
+  final Ref _ref;
+
+  AuthController({
+    required AuthRepository authRepository,
+    required Ref ref,
+  })  : _authRepository = authRepository,
+        _ref = ref,
+        super(false); // loading
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
-    user.fold((l) => showSnackBar(context, l.message),(r) => null);
+    state = false;
+    user.fold(
+      (l) => showSnackBar(context, l.message),
+      (userModel) => _ref.read(userProvider.notifier).update((state) => userModel),
+    );
   }
 }
