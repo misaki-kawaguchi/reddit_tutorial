45bda95859748481b67131e185e4861c3771c3dc
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-18 21:15:37 +0900

    ログインしている場合はHomeScreenを表示する

--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:reddit_tutorial/features/auth/login_screen.dart';
+import 'package:reddit_tutorial/features/auth/screens/login_screen.dart';
+import 'package:reddit_tutorial/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: LoginScreen()),
+});
+
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
 });
