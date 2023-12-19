a4d5ffd0618206dfad1658d6fb325d1f40a20c43
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-12-19 16:48:07 +0900

    Home画面にAppBarを追加 (#12)

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -10,6 +10,26 @@ class HomeScreen extends ConsumerWidget {
     final user = ref.watch(userProvider)!;
 
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: Center(
         child: Text(user.name),
       ),
