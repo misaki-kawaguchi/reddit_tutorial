b0a4dc42d61e9a1bba72de212c2f4b927b0856bd
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-12-19 21:00:34 +0900

    CreateCommunityScreenのUIを作成 (#14)

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -9,10 +9,57 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final communityNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    communityNameController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create c community'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text('Community name'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: communityNameController,
+              decoration: const InputDecoration(
+                hintText: 'r/Community_name',
+                filled: true,
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 21,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+              child: const Text(
+                'Create community',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
