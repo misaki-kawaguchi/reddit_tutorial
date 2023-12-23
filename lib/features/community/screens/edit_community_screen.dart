7bd90784c85d1a3a07e8857307ae99bef8d04d1a
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-23 14:40:37 +0900

    EditCommunityScreenのUIを作成

--- lib/features/community/screens/edit_community_screen.dart
+++ lib/features/community/screens/edit_community_screen.dart
@@ -1,5 +1,11 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/core/common/error_text.dart';
+import 'package:reddit_tutorial/core/common/loader.dart';
+import 'package:reddit_tutorial/core/constants/constants.dart';
+import 'package:reddit_tutorial/features/community/controller/community_controller.dart';
+import 'package:reddit_tutorial/theme/palette.dart';
 
 class EditCommunityScreen extends ConsumerStatefulWidget {
   const EditCommunityScreen({
@@ -16,6 +22,65 @@ class EditCommunityScreen extends ConsumerStatefulWidget {
 class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return ref.watch(getCommunityByNameProvider(widget.name)).when(
+          data: (community) => Scaffold(
+            backgroundColor: Palette.darkModeAppTheme.backgroundColor,
+            appBar: AppBar(
+              title: const Text('Edit Community'),
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Save'),
+                ),
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 200,
+                    child: Stack(
+                      children: [
+                        DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 4],
+                          strokeCap: StrokeCap.round,
+                          color: Palette.darkModeAppTheme.textTheme.bodyText2!.color!,
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: community.banner.isNotEmpty || community.banner == Constants.bannerDefault
+                                ? const Center(
+                                    child: Icon(
+                                      Icons.camera_alt_outlined,
+                                      size: 40,
+                                    ),
+                                  )
+                                : Image.network(community.banner),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 20,
+                          left: 20,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(community.avatar),
+                            radius: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          error: (error, stackTrace) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
